d13f870acc15d5ddfc853073a8496ac2f24f710b
Michael Webb <michael.webb@jisc.ac.uk>
2016-01-19 13:42:04 +0000

    no message

--- A1.js
+++ A1.js
@@ -1,83 +1,57 @@
-{
-    "version": "1.0.0",
-    "actor": {
-        "objectType": "Agent",
-        "name": "demouser",
-        "account": {
-            "name": "3",
-            "homePage": "http://moodle.data.alpha.jisc.ac.uk"
-        }
-    },
-    "verb": {
-        "id": "https://brindlewaye.com/xAPITerms/verbs/loggedin/",
-        "display": {
-            "en": "logged in to"
-        }
-    },
-    "context": {
-        "contextActivities": {
-            "grouping": [
-                {
-                    "objectType": "Activity",
-                    "id": "http://moodle.data.alpha.jisc.ac.uk/course/view.php?id=1",
-                    "definition": {
-                        "type": "http://lrs.learninglocker.net/define/type/moodle/course",
-                        "name": {
-                            "en": "University of Jisc Moodle"
-                        },
-                        "description": {
-                            "en": "A Moodle course"
-                        }
-                    }
-                }
-            ]
-        },
-        "platform": "Moodle",
-        "language": "en",
-        "extensions": {
-            "http://lrs.learninglocker.net/define/extensions/moodle_logstore_standard_log": {
-                "eventname": "\\core\\event\\user_loggedin",
-                "component": "core",
-                "action": "loggedin",
-                "objecttable": "user",
-                "objectid": "3",
-                "crud": "r",
-                "contextid": 1,
-                "contextlevel": 10,
-                "userid": "3",
-                "other": "a:1:{s:8:\"username\";s:8:\"demouser\";}",
-                "timecreated": 1453204740,
-                "origin": "web",
-                "ip": "86.156.130.212",
-                "object": "user"
-            },
-            "http://lrs.learninglocker.net/define/extensions/info": {
-                "https://moodle.org/": "2.8.9+ (Build: 20151110)",
-                "https://github.com/LearningLocker/Moodle-Log-Expander": "0.4.2",
-                "https://github.com/LearningLocker/Moodle-xAPI-Translator": "0.4.1",
-                "https://github.com/LearningLocker/xAPI-Recipe-Emitter": "0.4.3"
+{  
+   "actor":{  
+      "name":"3",
+      "account":{  
+         "homePage":"https://courses.alpha.jisc.ac.uk/moodle",
+         "name":"3"
+      }
+   },
+   "context":{  
+      "platform":"Moodle",
+      "language":"en",
+      "extensions":{  
+         "http://lrs.learninglocker.net/define/extensions/moodle_logstore_standard_log":{  
+            "eventname":"\\core\\event\\user_loggedin",
+            "timecreated":"2016-01-19 11:59:00+00:00",
+            "courseid":"0"
+         },
+         "http://lrs.learninglocker.net/define/extensions/info":"University of Jisc Moodle"
+      },
+      "contextActivities":{  
+         "grouping":[  
+            {  
+               "id":"https://courses.alpha.jisc.ac.uk/",
+               "definition":{  
+                  "type":"http://lrs.learninglocker.net/define/type/moodle/loggedin",
+                  "name":{  
+                     "en":"University of Jisc Moodle"
+                  },
+                  "description":{  
+                     "en":"Uni of Jisc Demo Moodle/"
+                  }
+               }
             }
-        }
-    },
-    "timestamp": "2016-01-19T11:59:00+00:00",
-    "object": {
-        "objectType": "Activity",
-        "id": "http://moodle.data.alpha.jisc.ac.uk/course/view.php?id=1",
-        "definition": {
-            "type": "http://lrs.learninglocker.net/define/type/moodle/course",
-            "name": {
-                "en": "University of Jisc Moodle"
-            },
-            "description": {
-                "en": "A Moodle course"
-            }
-        }
-    },
-    "authority": {
-        "objectType": "Agent",
-        "name": "Moodle",
-        "mbox": "mailto:hello@learninglocker.net"
-    },
-    "stored": "2016-01-19T11:55:38.129900+00:00",
-    "id": "e0c4cd4b-3581-4bb0-b4ae-befc602ae7d9"
-}
+         ]
+      }
+   },
+   "timestamp":"1969-12-31T16:33:36-08:00",
+   "verb":{  
+      "id":"https://brindlewaye.com/xAPITerms/verbs/loggedin/",
+      "display":{  
+         "en":"logged in to"
+      }
+   },
+   "object":{  
+      "id":"https://courses.alpha.jisc.ac.uk/",
+      "definition":{  
+         "type":"http://lrs.learninglocker.net/define/type/moodle/loggedin",
+         "name":{  
+            "en":"University of Jisc Moodle"
+         },
+         "description":{  
+            "en":"Uni of Jisc Demo Moodle/"
+         }
+      }
+   }
+},
+
